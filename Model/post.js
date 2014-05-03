1f3157396af1c60f8ee4b143c455414c1ff1c937
Jeremiah <AlienHoboken@users.noreply.github.com>
2014-05-03 16:53:50 -0400

    Adding skill module

--- Model/post.js
+++ Model/post.js
@@ -8,4 +8,4 @@ module.exports = function() {
 		location: {city: "", state: "", zip: ""},
 		skills: []
 	}
-}
+};
---
+++ Model/skill.js
@@ -0,0 +1,6 @@
+module.exports = function() {
+	return {
+		name: "",
+		trendiness: 0
+	}
+}
