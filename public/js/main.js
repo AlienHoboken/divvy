c17377a4b70b87f64c4a2b51edbe9802466b611c
Alex Sears <alexwsears@gmail.com>
2014-05-03 21:07:47 -0400

    Added some changes to edit account page

--- public/js/main.js
+++ public/js/main.js
@@ -7,15 +7,26 @@ $(document).ready(function() {
   $('#sign-up-toggle').click(function() {
     if ($('#sign-in').is(':visible')) {
       $('#sign-in').hide();
+      $('#sign-in-toggle').removeClass('selected');
     }
+
     $('#sign-up').slideToggle({ duration: 750 });
+    $(this).toggleClass('selected');
   });
 
   $('#sign-in-toggle').click(function() {
     if ($('#sign-up').is(':visible')) {
       $('#sign-up').hide();
+      $('#sign-up-toggle').removeClass('selected');
     }
+
     $('#sign-in').slideToggle({ duration: 750 });
+    $(this).toggleClass('selected');
+  });
+
+  $('.filter-group a').click(function() {
+    $('.filter-group a').removeClass('selected');
+    $(this).addClass('selected');
   });
 
   $.get('/account', { user: 1 }, function(data) {
