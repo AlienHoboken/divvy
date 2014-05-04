43f80f39b801d8019b2ad38b2680be312bdafb0b
Alex Sears <alexwsears@gmail.com>
2014-05-04 08:00:24 -0400

    Added people interested to single listing page.

--- Routes/posting.js
+++ Routes/posting.js
@@ -3,16 +3,17 @@ var Logic = require('../Model/logic');
 module.exports = function(db) {
 	return {
 		post : function(req,res) {
-			var id = req.query.id;
-			db.post.findOne({_id: id}, function(err, doc) {
-        if (doc) {
-          res.render('listing', {post: doc});
-        }
-        else {
-          res.redirect('/');
-        }
-		  }
-    );
+			// var id = req.query.id;
+			// db.post.findOne({_id: id}, function(err, doc) {
+   //      if (doc) {
+   //        res.render('listing', {post: doc});
+   //      }
+   //      else {
+   //        res.redirect('/');
+   //      }
+		 //  }
+   //  );
+            res.render('listing');
 	}
 }
 };
